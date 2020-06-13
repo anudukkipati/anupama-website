cdfa5d576e284fccd41c0c9ac7750326c842383e
anupama <anupamagadde@yahoo.com>
2020-06-13 15:34:58 -0500

    add header footer and about components

--- anupama-website/src/App.js
+++ anupama-website/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 //import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
+import Header from './components/Header.js'
+import NavBar from './components/NavBar.js'
+import Footer from './components/Footer.js'
+import About from './components/About.js'
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello, my name is Anupama
-      </header>
-    </div>
+    <Container>
+        <div className="App">
+        <NavBar  />
+           <Header />
+           <br />
+           <br />
+           <Switch>
+             <Route exact path="/" component={About} />
+           </Switch>
+
+           <Footer />
+        </div>
+
+    </Container>
+   
   );
 }
 
